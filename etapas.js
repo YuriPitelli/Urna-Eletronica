9ee71f73aee444903d803aa3df69ecca4ff2b30d
YuriPitelli <99697852+YuriPitelli@users.noreply.github.com>
2022-03-25 13:57:48 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,57 @@
+let etapas = [
+
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Jesse James',
+                partido: 'Texas',
+                fotos:[
+                    {url:'ver1-84.jpg', legenda: 'Vereador'}
+                ]
+            },
+
+            {
+                numero: '77222',
+                nome: 'John Wesley',
+                partido: 'Virginia',
+                fotos:[
+                    {url: 'ver2-84.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: '99',
+                nome: 'Trinity',
+                partido: 'Dunton',
+                vice: 'Meia lata',
+                fotos:[
+                    {url: 'pref1-84.jpg', legenda: 'Prefeito'},
+                    {url: 'vice1-84.png', legenda: 'Vice-prefeito', small: true}
+                ]
+            },
+
+            {
+                numero: '84',
+                nome: 'Clay Allison',
+                partido: 'Colorado',
+                vice: 'Dedin Ligero',
+                fotos:[
+                    {url:'pref2-84.jpg', legenda: 'Prefeita'},
+                    {url:'vice2-84.jpg', legenda: 'vice-Prefeito', small: true}
+                ]
+            },
+
+        ]
+
+    }
+
+];
